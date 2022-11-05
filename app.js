4289c6c073337ca13cd8bc9a41071e13fe870292
Mariya <116441490+BarsMariya@users.noreply.github.com>
2022-11-05 14:48:06 +0300

    Условный оператор

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 //const age = 21;
-const gender = 'Муж' // жен
-let age = 50;
+//const gender = 'Муж' // жен
+//let age = 50;
 //age = +age;
 
-if (!isNaN(+age)) {   
-    if (age > 18) {
-        console.log("Вам больше 18 лет");
-    } else if (age < 18) {
-        console.log("вам меньше 18 лет");
-    } else if (age === 18) {
-        console.log("вам 18 лет");
-    } else {
-        console.log('Укажите ваш возраст');
-    }
-} else {
-    console.log('Введите корректные данные')
-}
+// if (!isNaN(+age)) {   
+//     if (age > 18) {
+//         console.log("Вам больше 18 лет");
+//     } else if (age < 18) {
+//         console.log("вам меньше 18 лет");
+//     } else if (age === 18) {
+//         console.log("вам 18 лет");
+//     } else {
+//         console.log('Укажите ваш возраст');
+//     }
+// } else {
+//     console.log('Введите корректные данные')
+// }
 
-if (gender.toLocaleLowerCase() === 'муж'){
-    console.log('вам налево');
-}
-if (gender.toLocaleLowerCase() === 'жен'){
-    console.log('вам направо');
-}
+// if (gender.toLocaleLowerCase() === 'муж'){
+//     console.log('вам налево');
+// }
+// if (gender.toLocaleLowerCase() === 'жен'){
+//     console.log('вам направо');
+// }
+const isAdmin = true;
+
+console.log(`Добро пожаловать: ${
+    isAdmin 
+    ? `Администратор`
+    : `пользователь`
+}`);
