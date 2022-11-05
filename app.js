de52175c41bdc9e2affc5ac82617746eb37d6b26
Mariya <116441490+BarsMariya@users.noreply.github.com>
2022-11-05 13:53:07 +0300

    Добавили else в условие проверки возраста

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const age = 15;
+const age = 21;
 const gender = 'Муж' // жен
 if (age > 18) {
     console.log("Вам больше 18 лет");
+} else {
+    console.log('вам меньше 18 лет');
 }
+
 if (gender.toLocaleLowerCase() === 'муж'){
     console.log('вам налево');
 }
