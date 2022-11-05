897b3eeb08cbe54712912107bf9421161b001f90
Mariya <116441490+BarsMariya@users.noreply.github.com>
2022-11-05 13:49:07 +0300

    Условия if

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('hello')
+const age = 15;
+const gender = 'Муж' // жен
+if (age > 18) {
+    console.log("Вам больше 18 лет");
+}
+if (gender.toLocaleLowerCase() === 'муж'){
+    console.log('вам налево');
+}
+if (gender.toLocaleLowerCase() === 'жен'){
+    console.log('вам направо');
+}
